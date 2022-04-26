797ce7f222ef5c1f9a77c1554848e1538d8751c0
SrGuiillermo <guillermonvl1@gmail.com>
2022-04-26 14:35:06 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 config = {
-    "tmi" : "",
-    "channels" : [""],
-    "authorized" : [""],
-    "propietary" : [""],
+    "tmi" : "oauth:05kn4wpfeuj2a7dv0az1mflno4fq0q",
+    "channels" : ["SrGuillermo", "SrGuillermoBETS", "Okiss", "ElmiilloR", "ffaka", "AdcShiiro"],
+    "authorized" : ["srguillermo", "srguillermobets", "hyboria3", "okiss", "nick_namess", "feiipito", "alejandro_pf"],
+    "propietary" : ["srguillermo"],
 }
